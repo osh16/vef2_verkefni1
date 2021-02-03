93336b9b36a7e70ec557663eb152f3f086d27298
Oskar Hoskuldsson <osh16@hi.is>
2021-02-03 12:26:59 +0000

    sass loksins ad virka

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.locals.time = require('./src/time');
 app.locals.teitur = "HELLO TEITUR";
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
